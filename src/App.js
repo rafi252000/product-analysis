44a600ed5c10e559da7fd700fbf930c3309c92e1
rafi252000 <rafiuddin.cse.pciu@gmail.com>
2022-04-14 01:12:26 +0600

    added nav

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
       <Navebar></Navebar>
-      <h1 className='text-6xl text-left'>NEW SMART WATCH</h1>
-      <h1 className='text-6xl text-left '>Classy! Get in Reasonable Price</h1>
-      <img src="/public/piaget-1636410151.jpg" alt="" />
+      <h1 className='text-6xl text-left font-bold mt-20 ml-8'>NEW SMART WATCH</h1>
+      <h1 className='text-6xl text-left font-bold text-blue-600  mt-5 ml-8'>Classy! Get in Reasonable Price</h1>
+      <div>
+      <img src="piaget-1636410151.jpg" alt="" />
+      </div>
     </div>
   );
 }
--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = (props) => {
     const {name,link}=props.route;
     return (
-        <li className='mr-12'>
+        <li className='mr-12 text-white py-4'>
             <a href={link}>{name}</a>
         </li>
     );
